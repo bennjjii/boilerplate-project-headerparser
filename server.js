87bdf830a57fbe94209d22c8122b231f4a5b514a
bennjjii <>
2021-05-14 21:42:05 +0000

    Implement route /api/whoami

--- server.js
+++ server.js
@@ -20,10 +20,14 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get("/api/whoami", (req, res)=>{
+  const response = {
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  }
+  res.json(response)
+})
 
 
 
